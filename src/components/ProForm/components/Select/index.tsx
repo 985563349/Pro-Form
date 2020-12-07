aa0f2472779b8e1d5ac904aa01ef6d2c603c3c91
Jie <jie985563349@outlook.com>
2020-12-07 15:18:11 +0800

    扩展select组件

--- src/components/ProForm/components/Select/index.tsx
+++ src/components/ProForm/components/Select/index.tsx
@@ -1,28 +1,33 @@
 import { FunctionComponent } from 'react';
 import { Select } from 'antd';
-import { SelectProps } from 'antd/lib/select';
+import { SelectProps, OptionProps } from 'antd/lib/select';
 
 import ProFormItem, { ProFormItemProps } from '../FormItem';
 
 export interface ProFormSelectProps extends ProFormItemProps {
-  options: any[];
+  options: Omit<OptionProps, 'children'>[];
+  placeholder?: string;
   fieldProps?: SelectProps<any>;
 }
 
 const ProFormSelect: FunctionComponent<ProFormSelectProps> = (props) => {
-  const { fieldProps, options, ...formProps } = props;
+  const { options, placeholder, fieldProps, ...formProps } = props;
 
   return (
     <ProFormItem {...formProps}>
-      <Select {...fieldProps}>
-        {options?.map(({ label, value }) => (
-          <Select.Option key={value} value={value}>
+      <Select {...fieldProps} placeholder={placeholder}>
+        {options?.map(({ label, value, ...props }) => (
+          <Select.Option {...props} key={value} value={value}>
             {label}
           </Select.Option>
         ))}
       </Select>
     </ProFormItem>
   );
+};
+
+ProFormSelect.defaultProps = {
+  placeholder: '请选择',
 };
 
 export default ProFormSelect;
