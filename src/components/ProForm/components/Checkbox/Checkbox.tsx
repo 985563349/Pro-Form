6142b09df4d11f1bbac7de7876edc9e10bc58d8d
Jie <jie985563349@outlook.com>
2020-12-25 18:26:23 +0800

    proFormCheckbox添加事件处理

--- src/components/ProForm/components/Checkbox/Checkbox.tsx
+++ src/components/ProForm/components/Checkbox/Checkbox.tsx
@@ -5,18 +5,20 @@ import { CheckboxProps } from 'antd/lib/checkbox';
 import ProFormItem, { ProFormItemProps } from '../Form/FormItem';
 import ProFormCheckboxGroup from './CheckboxGroup';
 
-export interface ProFormCheckboxProps extends ProFormItemProps {
+export interface ProFormCheckboxProps
+  extends Pick<CheckboxProps, 'onChange'>,
+    ProFormItemProps {
   fieldProps?: CheckboxProps;
 }
 
 const ProFormCheckbox: FunctionComponent<ProFormCheckboxProps> & {
   Group: typeof ProFormCheckboxGroup;
 } = (props) => {
-  const { children, fieldProps, ...formProps } = props;
+  const { fieldProps, children, onChange, ...formProps } = props;
 
   return (
     <ProFormItem {...formProps} valuePropName="checked">
-      <Checkbox {...fieldProps} children={children} />
+      <Checkbox {...fieldProps} children={children} onChange={onChange} />
     </ProFormItem>
   );
 };
--- src/components/ProForm/components/Checkbox/CheckboxGroup.tsx
+++ src/components/ProForm/components/Checkbox/CheckboxGroup.tsx
@@ -4,19 +4,20 @@ import { CheckboxGroupProps } from 'antd/lib/checkbox';
 
 import ProFormItem, { ProFormItemProps } from '../Form/FormItem';
 
-export interface ProFormCheckboxGroupProps extends ProFormItemProps {
-  options?: CheckboxGroupProps['options'];
+export interface ProFormCheckboxGroupProps
+  extends Pick<CheckboxGroupProps, 'options' | 'onChange'>,
+    ProFormItemProps {
   fieldProps?: CheckboxGroupProps;
 }
 
 const ProFormCheckboxGroup: FunctionComponent<ProFormCheckboxGroupProps> = (
   props
 ) => {
-  const { options, fieldProps, ...formProps } = props;
+  const { options, fieldProps, onChange, ...formProps } = props;
 
   return (
     <ProFormItem {...formProps}>
-      <Checkbox.Group options={options} {...fieldProps} />
+      <Checkbox.Group {...fieldProps} options={options} onChange={onChange} />
     </ProFormItem>
   );
 };
